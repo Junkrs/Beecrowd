891f97a1d845247262134c7c5247181cc7cc613b
Gabriel Junqueira <junqueirabr158@gmail.com>
2024-04-11 13:18:01 -0300

    Bee 1001 - C Solution

---
+++ Bee 1001 - Extremely Basic/ExtremelyBasic.c
@@ -0,0 +1,12 @@
+//Bee 1001 - Extremely Basic
+
+#include <stdio.h>
+ 
+int main() {
+    int A, B;
+    scanf("%d %d", &A, &B);
+    
+    int X = A + B;
+    printf("X = %d\n", X);
+    return 0;
+}
