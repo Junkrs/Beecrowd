754e9fc30b5b5b6900d0e9e331f6152130cc4f60
Gabriel Junqueira <junqueirabr158@gmail.com>
2024-04-11 14:41:03 -0300

    Bee 1007 - C Solution

---
+++ Bee 1007 - Difference/Difference.c
@@ -0,0 +1,12 @@
+//Bee 1007 - Difference
+
+#include <stdio.h>
+int main(){
+	int A, B, C, D, DIFERENCA;
+	
+	scanf("%i %i %i %i", &A, &B, &C, &D);
+	DIFERENCA = (A * B) - (C * D);
+	
+	printf("DIFERENCA = %i\n", DIFERENCA);
+	return 0;
+}
