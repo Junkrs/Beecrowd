3665a1d43efbba6ab46ebf0754fe3a34a424af05
Gabriel Junqueira <junqueirabr158@gmail.com>
2024-04-11 14:34:11 -0300

    Bee 1006 - C Solution

---
+++ Bee 1006 - Average 2/Average2.c
@@ -0,0 +1,12 @@
+//Bee 1006 - Average 2
+
+#include <stdio.h>
+int main(){
+	float A, B, C, MEDIA;
+	
+	scanf("%f %f %f", &A, &B, &C);
+	MEDIA = ((A * 2 ) + (B * 3) + (C * 5))/10;
+	
+	printf("MEDIA = %.1f\n", MEDIA);
+	return 0;
+}
