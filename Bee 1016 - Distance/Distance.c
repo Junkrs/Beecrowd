c3d332b8054df8bbf1080808383db761f0cca27f
Gabriel Junqueira <junqueirabr158@gmail.com>
2024-04-14 15:07:09 -0300

    Bee 1016 - C/Pyhton Solution

---
+++ Bee 1016 - Distance/Distance.c
@@ -0,0 +1,12 @@
+//Bee 1016 - Distance
+
+#include <stdio.h>
+ 
+int main() {
+    int km;
+    scanf("%i", &km);
+    
+    printf("%i minutos\n", (km * 2));
+    
+    return 0;
+}
---
+++ Bee 1016 - Distance/Distance.py
@@ -0,0 +1,6 @@
+#Bee 1016 - Distance
+
+km = int(input())
+min = (km * 2)
+
+print('%i minutos' % min)
