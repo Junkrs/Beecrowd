586342be7f7a684163dee1251df5d9829ce57e03
Gabriel Junqueira <junqueirabr158@gmail.com>
2024-04-22 16:20:05 -0300

    1000 - C++/Java Solution

---
+++ Bee 1000 - Hello World!/HelloWorld.cpp
@@ -0,0 +1,10 @@
+//Bee 1000 - Hello World!
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "Hello World!\n";
+    return 0;
+}
---
+++ Bee 1000 - Hello World!/Main.java
@@ -0,0 +1,12 @@
+//Bee 1000 - Hello World!
+
+import java.io.IOException; 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        System.out.println("Hello World!");
+ 
+    }
+ 
+}
