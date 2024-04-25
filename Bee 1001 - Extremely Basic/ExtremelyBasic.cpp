86ae1b50a654cd9c16ead93123b27b2a078865ec
Gabriel Junqueira <junqueirabr158@gmail.com>
2024-04-25 13:36:30 -0300

    1001 - C++ solution

---
+++ Bee 1001 - Extremely Basic/ExtremelyBasic.cpp
@@ -0,0 +1,16 @@
+//Bee 1001 - Extremely Basic
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B;
+
+    cin >> A >> B;
+
+    int X;
+    X = A + B;
+
+    cout << "X = " << X << endl;
+    return 0;
+}
