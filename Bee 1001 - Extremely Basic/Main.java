95b4a13e33544a69a2f26479008c8e7d7c3a0601
Gabriel Junqueira <junqueirabr158@gmail.com>
2024-05-02 10:18:39 -0300

    1001 - Java solution

---
+++ Bee 1001 - Extremely Basic/Main.java
@@ -0,0 +1,20 @@
+//Bee 1001 - Extremely Basic
+
+import java.io.IOException; 
+import java.util.*;
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner scn = new Scanner(System.in);
+        int A, B;
+
+        A = scn.nextInt();
+        B = scn.nextInt();
+
+        int X = A + B;
+
+        System.out.println("X = " + X + "\n");
+ 
+    }
+ 
+}
