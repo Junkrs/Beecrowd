2e14e9ee8e3e6192425273e1819a1b6ffd97b13d
Gabriel Junqueira <junqueirabr158@gmail.com>
2024-05-02 11:35:01 -0300

    1035 - C++ solution

---
+++ Bee 1038 - Snack/Snack.cpp
@@ -0,0 +1,24 @@
+//Bee 1038 - Snack
+
+#include <iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+    int X, Y;
+    cin >> X >> Y;
+    
+    if(X == 1){
+        cout << "Total de R$ " << fixed << setprecision(2) << 4.00 * Y << endl;
+    } else if ( X == 2) {
+        cout << "Total de R$ " << fixed << setprecision(2) << 4.50 * Y << endl;
+    } else if ( X == 3 ) {
+        cout << "Total de R$ " << fixed << setprecision(2) << 5.00 * Y << endl;
+    } else if ( X == 4 ) {
+        cout << "Total de R$ " << fixed << setprecision(2) << 2.00 * Y << endl;
+    } else if ( X == 5 ) {
+        cout << "Total de R$ " << fixed << setprecision(2) << 1.50 * Y << endl;
+    }    
+    return 0;
+}
